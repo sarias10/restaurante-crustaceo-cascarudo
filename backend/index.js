f1dad52a2819db10c2cd57bef3c968482394108a
Sergio Arias <s-andres140@hotmail.com>
2024-12-30 21:22:43 -0500

    feat: :hammer: change name in index route

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const local = process.env.LOCAL === 'true'
 
-app.get('/', (req, res) => {
+app.get('/hola_mundo', (req, res) => {
   res.send('Hello from github actions!');
 });
 
