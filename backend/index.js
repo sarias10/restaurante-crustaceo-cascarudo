f92252edb69bcce392b3db8b27843fcb94305699
Sergio Arias <s-andres140@hotmail.com>
2024-12-30 20:12:08 -0500

    feat: :hammer: add hello world route in backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import serverless from 'serverless-http';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const local = process.env.LOCAL === 'true'
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+export const handler = serverless(app);
+
+if(local) {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
