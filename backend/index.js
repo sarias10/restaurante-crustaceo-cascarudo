6e81bbc52431643fe94d98a9e32d19d68e59132e
Sergio Arias <s-andres140@hotmail.com>
2024-12-30 22:13:48 -0500

    feat: :bulb: add comment blocks

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,8 @@ import serverless from 'serverless-http';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
-const local = process.env.LOCAL === 'true'
+/* const port = process.env.PORT || 3000;
+const local = process.env.LOCAL === 'true' */
 
 app.get('/hola_mundo', (req, res) => {
   res.send('Hello from github actions!');
@@ -18,8 +18,8 @@ app.get('/otro', (req, res) => {
 
 export const handler = serverless(app);
 
-if(local) {
+/* if(local) {
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
-}
+} */
